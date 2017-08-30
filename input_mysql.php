5db453a719f30dd855a970cf758b07233027a75d
fanhua <1824454842@qq.com>
2017-08-30 20:48:43 +0800

    form表单输入数据提交数据库

---
+++ input_mysql.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input into MySQL</title>
+</head>
+<body>
+	<form method="post" action="insert.php">
+		<label>Firstname:</label>
+		<input name="firstname" type="text" ><br>
+		<label>Lastname:</label>
+		<input name="lastname" type="text" ><br>
+		<label>Age:</label>
+		<input name="age" type="text" ><br>
+		<input type="submit">
+	</form>
+</body>
+</html>
