057aa07f8d6d8d29880b868986b72c61ea3e04ef
fanhua <1824454842@qq.com>
2017-09-03 20:45:42 +0800

    ajax后台判断文件

--- ajax.php
+++ ajax.php
@@ -1,29 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>leetcode</title>
-</head>
-<body>
-	<p id="demo">点击这个按钮，获得您的坐标：</p>
-	<button onclick="getLocation()">试一下</button>
-	<script type="text/javascript">
-		function AjaxDoc(){
-			var xmlHttp; 
-			if (window.XMLHttpRequest) {
-				xmlHttp = new XMLHttpRequest();
-			}else{
-				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-			}
-			xmlHttp.onreadystatechange = function(){
-				if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-					alert(xmlHttp.responseText);
+<?php
+	// Fill up array with names
+	$a[]="Anna";
+	$a[]="Brittany";
+	$a[]="Cinderella";
+	$a[]="Diana";
+	$a[]="Eva";
+	$a[]="Fiona";
+	$a[]="Gunda";
+	$a[]="Hege";
+	$a[]="Inga";
+	$a[]="Johanna";
+	$a[]="Kitty";
+	$a[]="Linda";
+	$a[]="Nina";
+	$a[]="Ophelia";
+	$a[]="Petunia";
+	$a[]="Amanda";
+	$a[]="Raquel";
+	$a[]="Cindy";
+	$a[]="Doris";
+	$a[]="Eve";
+	$a[]="Evita";
+	$a[]="Sunniva";
+	$a[]="Tove";
+	$a[]="Unni";
+	$a[]="Violet";
+	$a[]="Liza";
+	$a[]="Elizabeth";
+	$a[]="Ellen";
+	$a[]="Wenche";
+	$a[]="Vicky";
+
+	$q = $_GET["q"];
+	if (strlen($q)) {
+		$hint = "";
+		for ($i=0; $i < count($a); $i++) { 
+			if (strtolower($q) == strtolower(substr($a[$i],0,strlen($q)))) {
+				if ($hint == "") {
+					$hint = $a[$i];
+				}
+				else{
+					$hint = $hint . "," . $a[$i];
 				}
 			}
-			xmlHttp.open("GET","",true);
-			xmlHttp.send();
 		}
-		
-	</script>
-</body>
-</html>
+	}
+	if (!$hint) {
+		$response = "no suggestion!";
+	}else{
+		$response = $hint;
+	}
+	echo "$response";
+
+?>
